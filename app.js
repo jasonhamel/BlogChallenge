2e1c6a802d8a547cec2867a6937dcd32c42171ac
Jason <github@jasongetsemail.com>
2023-09-04 20:25:09 -0400

    Completed challenge

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import _ from "lodash";
+import mongoose from "mongoose";
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -12,16 +13,27 @@ const contactContent =
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+await mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {
+  useNewUrlParser: true,
+});
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model("Post", blogSchema);
+
 let posts = [];
 
-app.get("/", function (req, res) {
+app.get("/", async function (req, res) {
+  const postSearchResults = await Post.find();
   res.render("home", {
     startingContent: homeStartingContent,
-    posts: posts,
+    posts: postSearchResults,
   });
 });
 
@@ -37,29 +49,25 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function (req, res) {
-  const post = {
+app.post("/compose", async function (req, res) {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
+  });
 
-  posts.push(post);
+  await post.save();
 
   res.redirect("/");
 });
 
 app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content,
-      });
-    }
+  const toFind = req.params.postName;
+  console.log(toFind);
+  Post.findOne({ _id: toFind }).then((foundPost) => {
+    res.render("post", {
+      title: foundPost.title,
+      content: foundPost.content,
+    });
   });
 });
 
